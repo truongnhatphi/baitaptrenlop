80d552f0393d0967e985c7e29a67736e655087f7
truongnhatphi <55117983+truongnhatphi@users.noreply.github.com>
2020-04-23 10:43:39 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,51 +1,56 @@
-  <script>
-    function getLinkJSONpt2() {
-        var a,b,c;
-        a = document.getElementById("inputa").value;
-        b = document.getElementById("inputb").value;
-        c = document.getElementById("inputc").value;
-        window.open("api.php/checkptbac2/"+a+"/"+b+"/"+c);
-    }
-    function getLinkJSONyear() {
-        var a;
-        a = document.getElementById("inputyear").value;
-        window.open("api.php/checkyear/"+a);
-    }
-    function getData()
-    {
-        document.getElementById("txt_apijsonpt2").innerHTML = window.location.host +"/CloudPhpEX/api.php/checkptbac2/a/b/c";
-        document.getElementById("txt_apijsonyear").innerHTML = window.location.host +"/CloudPhpEX/api.php/checkyear/year";
-    }
+<script>
+    function getdata()
+	{
+		var a;
+		a = <?php echo $1?>;
+		document.getElementById("ketqua").value = a;
+	}
 </script>
 
 <!DOCTYPE html>
 <html lang="vi" xmlns="http://www.w3.org/1999/xhtml">
 <html>
 <head>
-    <title>Ex restful api</title>
+    <title>Trương Nhật Phi</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<h1>Giải phương trình bậc 2</h1>
-<a>ax² + bx + c = 0</a>
-<form>
-    <label for="fname">a</label><br>
-    <input type="number" id="inputa" name="fname"><br>
-    <label for="lname">b</label><br>
-    <input type="number" id="inputb" name="lname"><br>
-    <label for="lname">c</label><br>
-    <input type="number" id="inputc" name="lname"><br>
-    <input type ="button" name="OKE" value="GETJSON" onclick="getLinkJSONpt2()">
-</form>
-<h3>Api json pt2: </h3><h4 id="txt_apijsonpt2"></h4>
-<h1>Kiểm tra năm nhuần</h1>
-<form>
-    <label for="lname">Nhập năm</label><br>
-    <input type="number" id="inputyear" name="lname"><br>
-    <input type ="button" name="OKE" value="GETJSON" onclick="getLinkJSONyear()">
-</form>
-<h3>Api json pt2: </h3><h4 id="txt_apijsonyear"></h4>
-<script>getData();</script>
-</body>
-</html>
+<h1>Xét Tam giác :</h1>
+<?php
+	if(isset($_POST["a"]) && isset($_POST["b"]) && isset($_POST["c"]))
+		{
+			$a = $_POST["a"];
+			$b = $_POST["b"];
+			$c = $_POST["c"];
+			if( $a +$b > $c && $a+$c>$b && $b+$c > $a)
+			{
+				if($a==$b && $b==$c)
+				$1 = "Tam Giác Đều ";
+				elseif($a==$b || $a==$c || $b==$c )
+				$1 = "Tam Giác Cân ";
+				elseif(pow($a,2)==pow($b,2)+pow($c,2)||pow($b,2)==pow($c,2)+pow($c,2)|| pow($c,2)==pow($b,2)+pow($a,2))
+				$1 = "Tam Giác Vuông ";
+				elseif(($a==$b || $a==$c || $b==$c ) && (pow($a,2)==pow($b,2)+pow($c,2)||pow($b,2)==pow($c,2)+pow($c,2)|| pow($c,2)==pow($b,2)+pow($a,2)))
+				$1 = "Tam Giác Vuông Cân ";
+				else
+				$1 = "Tam Giác Thường ";
+			}
+			else
+			$1 = " Đây không phải tam giác ";
+		}
+		
+?>
+<div> Nhập canh A </div>
+<input name="a" type="text" id ="a" value="<?php echo $_POST["a"];?>" />
+<br>
+<div> Nhập canh B </div>
+<input name="b" type="text" id ="b" value="<?php echo $_POST["b"];?>" />
+<br>
+<div> Nhập canh C </div>
+<input name="c" type="text" id ="c" value="<?php echo $_POST["c"];?>" />
+<input name="ketqua">
+<input type="button" name="check" onclick="getdata()" />
+
+
+
